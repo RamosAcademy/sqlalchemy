3d6941e3fd265f6a4cbd3c3bbd439b4e17e483bb
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2022-05-22 08:58:40 -0400

    init sqlalchemy

---
+++ app.py
@@ -0,0 +1,8 @@
+# import models
+# main menu - add, search, analysis, exit, view options
+# add books to the db
+# edit books
+# delete books
+# search function
+# data cleaning
+# loop runs program
---
+++ models.py
@@ -0,0 +1,4 @@
+# create a db
+# books.db
+# create a model
+# title, author, date published, price
