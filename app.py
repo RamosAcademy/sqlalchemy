e32e44772f398afc9037049fe7a5bf8513d7fcfd
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2022-05-22 13:17:36 -0400

    able to analyze books

--- app.py
+++ app.py
@@ -214,7 +214,20 @@ def app():
                 time.sleep(1.5)
         elif choice == '4':
             '''book analysis'''
-            pass
+            oldest_book = session.query(Book).order_by(
+                Book.published_date).first()
+            newest_book = session.query(Book).order_by(
+                Book.published_date.desc()).first()
+            total_books = session.query(Book).count()
+            python_books = session.query(Book).filter(
+                Book.title.like('%Python%')).count()
+            print(f'''
+                \n*** BOOK ANALYSIS ***
+                \rOldest Book: {oldest_book}
+                \rNewest Book: {newest_book}
+                \rTotal Books: {total_books}
+                \rPython Books: {python_books}''')
+            input('\nPress enter to return to the main menu.')
         else:
             '''exit'''
             print('GOODBYE')
