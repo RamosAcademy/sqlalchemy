01d19ecbfcd25e499db49a23f634d8267e7c1464
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2022-05-22 09:12:05 -0400

    created books.db

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from models import (Base, session,
+                    Book, engine)
+
 # import models
 # main menu - add, search, analysis, exit, view options
 # add books to the db
@@ -6,3 +9,7 @@
 # search function
 # data cleaning
 # loop runs program
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
