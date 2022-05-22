a4ae5a949ec291c65082aa60af87f8d8712289ca
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2022-05-22 09:27:14 -0400

    create app menu

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from models import (Base, session,
                     Book, engine)
 
-# import models
+
+def menu():
+    while True:
+        print('''
+        \nPROGRAMMING BOOKS
+        \r1. Add book
+        \r2. View all books
+        \r3. Search for book
+        \r4. Book analysis
+        \r5. Exit''')
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2', '3', '4', '5']:
+            return choice
+        else:
+            input('''
+            \rPlease choose one of the options above.
+            \rA number from 1 - 5.
+            \rPress Enter to try again.''')
+
 # main menu - add, search, analysis, exit, view options
 # add books to the db
 # edit books
@@ -11,5 +29,30 @@ from models import (Base, session,
 # loop runs program
 
 
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+
+        # this is slow... refactor to use an indexed dict obj.
+        if choice == '1':
+            '''add book'''
+            pass
+        elif choice == '2':
+            '''view books'''
+            pass
+        elif choice == '3':
+            '''search book'''
+            pass
+        elif choice == '4':
+            '''book analysis'''
+            pass
+        else:
+            '''exit'''
+            print('GOODBYE')
+            app_running = False
+
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    app()
