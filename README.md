870bf505a346f60c5df9e8010396a63049710b01
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2022-05-23 11:20:37 -0400

    Add the data from CSV into the database

---
+++ README.md
@@ -0,0 +1,5 @@
+Code adapted from this project: https://teamtreehouse.com/library/project-overview-8
+
+To the best of my current ability I optimized data structures and abstracted helper functions out of the app.py file to ease code readability.
+
+Having a solid foundation ("legacy code") to work with I instead focused on making improvements and writing more pythonic code.
--- models.py
+++ models.py
@@ -1,27 +1,21 @@
-from curses import echo
-from sqlalchemy import (create_engine, Column,
-                        Integer, String, Date)
+from sqlalchemy import (create_engine, Column, Integer, String, Date)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///books.db', echo=False)
+engine = create_engine('sqlite:///inventory.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
 
-class Book(Base):
-    __tablename__ = 'books'
+class Product(Base):
+    __tablename__ = 'inventory'
 
-    id = Column(Integer, primary_key=True)
-    title = Column('Title', String)
-    author = Column('Author', String)
-    published_date = Column('Published', Date)
-    price = Column('Price', Integer)
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column('Name', String)
+    product_quantity = Column('Quantity', Integer)
+    product_price = Column('Price', Integer)
+    date_updated = Column('Date', Date)
 
     def __repr__(self) -> str:
-        return f'Title: {self.title}. Author: {self.author}, Published: {self.published_date}, Price: {self.price}'
-# create a db
-# books.db
-# create a model
-# title, author, date published, price
+        return f'Product: {self.product_name}. Quantity: {self.product_quantity}, Price: {self.product_price / 100}, Updated On: {self.date_updated}'
